2fa1828e9e399eecc43481746f1e3304dda68d09
Leeyeonkyung <37022442+Leeyeonkyung@users.noreply.github.com>
2018-03-27 14:37:16 +0900

    최종

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-function requestCallbackFunction (req, res) {
+/*function requestCallbackFunction (req, res) {
   // if(req.url === '/favicon.ico') {
   //   console.log('Favicon was requested');
   // } else {
@@ -9,9 +9,19 @@ function requestCallbackFunction (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }
-
+*/
 var server = http.createServer(
-  requestCallbackFunction
-);
+  function(req, res){
+    //RES는 웹에서 요청 내용
+     if(req.url === '/favicon.ico') {
+       //URL로 요청이 들어왔으면
+     console.log('Favicon was requested');
+    } else {
+      console.log('hello');
+   }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+  }
+); //이게다 콜백함수이다.
 
 server.listen(8080);
